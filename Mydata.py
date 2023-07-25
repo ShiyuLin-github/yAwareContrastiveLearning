b695edb83b80d45f5491b4f3b4c87f7b308be46f
ShiyuLin-github <1499901128@qq.com>
2023-07-25 15:49:19 +0800

    read my dataset(new)

---
+++ Mydata.py
@@ -0,0 +1,46 @@
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import nibabel as nib
+import numpy as np
+import pandas as pd
+
+
+class MyDataset(Dataset):
+    def __init__(self, root_dir, csv_dir, transform=None):
+        self.root_dir = root_dir
+        self.samples = os.listdir(root_dir)
+        self.csv_file = pd.read_csv(csv_dir)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sample_path = os.path.join(self.root_dir, self.samples[idx])
+
+        # Load each MRI sequence from the nii files,-6是除去文件夹名称里的_nifti
+        t1 = nib.load(os.path.join(sample_path, self.samples[idx][:-6] + '_T1.nii.gz')).get_fdata()
+        t1c = nib.load(os.path.join(sample_path, self.samples[idx][:-6] + '_T1c.nii.gz')).get_fdata()
+        t2 = nib.load(os.path.join(sample_path, self.samples[idx][:-6] + '_T2.nii.gz')).get_fdata()
+        flair = nib.load(os.path.join(sample_path, self.samples[idx][:-6] + '_FLAIR.nii.gz')).get_fdata()
+        asl = nib.load(os.path.join(sample_path, self.samples[idx][:-6] + '_ASL.nii.gz')).get_fdata()
+
+        # Combine the four MRI sequences into a single input tensor
+        # input_tensor = torch.Tensor(np.stack([t1, t1c, t2, flair, asl], axis=0))
+        input_tensor = torch.Tensor(t1)
+        # input_tensor = input_tensor.permute(0,3,1,2) #将维度重排为 [C, D, H, W]，有了transform就不需要在这里重排了，这个代码要求数据格式为[C, H, W, D]这里就不进行permute了
+        if self.transform:
+            input_tensor = self.transform(input_tensor)
+
+        # 从csv文件中读取label
+        # 原ID比csv文件中的ID多了个0，比如UCSF-PDGM-0004，csv中是UCSF-PDGM-004。所以要修改一下
+        id = self.samples[idx][:-6]
+        id_fit = id[0:-4] + id[-3:]
+        label = self.csv_file.loc[self.csv_file['ID'] == id_fit, 'WHO CNS Grade'].values[0]
+        label = label - 2  # 从[2,3,4]转为[0,1,2]
+        # PyTorch会自动把整数型的label转为one-hot型，用于计算CE loss这里需要确保label是从0开始的,from深入浅出pytorch
+
+        # Return the input tensor and any additional labels or targets
+        return input_tensor, label  # label是你的样本的标签，需要自己定义
--- dataset.py
+++ dataset.py
@@ -3,12 +3,18 @@ import numpy as np
 import pandas as pd
 import torch
 from augmentations import Transformer, Crop, Cutout, Noise, Normalize, Blur, Flip
+from Mydata import MyDataset
 
 class MRIDataset(Dataset):
 
     def __init__(self, config, training=False, validation=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert training != validation
+        #改：从Mydata中读取已写好的dataset
+        self.custom_dataset = MyDataset(root_dir='I:/UCSF-PDGM-v3/PKG - UCSF-PDGM-v3/Mydeepmedic/UCSF-PDGM-v3',csv_dir='UCSF-PDGM-metadata_v2.csv')
+        self.train_size = int(len(self.custom_dataset) * 0.7)
+        self.test_size = len(self.custom_dataset) - self.train_size
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.custom_dataset, [self.train_size, self.test_size])
 
         self.transforms = Transformer()
         self.config = config
@@ -31,14 +37,18 @@ class MRIDataset(Dataset):
                                      probability=1)
 
         if training:
-            self.data = np.load(config.data_train)
-            self.labels = pd.read_csv(config.label_train)
-        elif validation:
-            self.data = np.load(config.data_val)
-            self.labels = pd.read_csv(config.label_val)
+            #改：在之后getitem中处理数据问题
+            # self.data = np.load(config.data_train)
+            # self.labels = pd.read_csv(config.label_train)
+            self.training = True
 
-        assert self.data.shape[1:] == tuple(config.input_size), "3D images must have shape {}".\
-            format(config.input_size) #反斜杠是换行符，.format是用法，.\只是刚好巧了在一块
+        elif validation:
+            # self.data = np.load(config.data_val)
+            # self.labels = pd.read_csv(config.label_val)
+            self.validation = True
+
+        # assert self.data.shape[1:] == tuple(config.input_size), "3D images must have shape {}".\
+        #     format(config.input_size) #反斜杠是换行符，.format是用法，.\只是刚好巧了在一块
 
     def collate_fn(self, list_samples):
         list_x = torch.stack([torch.as_tensor(x, dtype=torch.float) for (x, y) in list_samples], dim=0)
@@ -47,15 +57,29 @@ class MRIDataset(Dataset):
         return (list_x, list_y)
 
     def __getitem__(self, idx):
+        if self.training:
+            #在此处getitem处改代码使得能用上之前的代码
+            data, labels = self.train_dataset[idx]
+            # For a single input x, samples (t, t') ~ T to generate (t(x), t'(x))
+            np.random.seed()
+            x1 = self.transforms(self.data)
+            x2 = self.transforms(self.data)
+            # labels = self.labels[self.config.label_name].values[idx]
+            x = np.stack((x1, x2), axis=0)
+            return (x, labels)
+        elif self.validation:
+            data, labels = self.test_dataset[idx]
+            # For a single input x, samples (t, t') ~ T to generate (t(x), t'(x))
+            np.random.seed()
+            x1 = self.transforms(self.data)
+            x2 = self.transforms(self.data)
+            # labels = self.labels[self.config.label_name].values[idx]
+            x = np.stack((x1, x2), axis=0)
+            return (x, labels)
 
-        # For a single input x, samples (t, t') ~ T to generate (t(x), t'(x))
-        np.random.seed()
-        x1 = self.transforms(self.data[idx])
-        x2 = self.transforms(self.data[idx])
-        labels = self.labels[self.config.label_name].values[idx]
-        x = np.stack((x1, x2), axis=0)
-
-        return (x, labels)
 
     def __len__(self):
-        return len(self.data)
+        if self.training:
+            return len(self.train_dataset)
+        elif self.validation:
+            return len(self.test_dataset)
